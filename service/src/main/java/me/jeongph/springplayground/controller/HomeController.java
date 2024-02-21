64afbff5544078691646ad2218eee059d21813b0
JeongUk Park <dnr1105@gmail.com>
2024-02-21 23:19:58 +0900

    feat: 임시 코드 최신화

--- service/src/main/java/me/jeongph/springplayground/controller/HomeController.java
+++ service/src/main/java/me/jeongph/springplayground/controller/HomeController.java
@@ -1,6 +1,7 @@
 package me.jeongph.springplayground.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "Hello, this is Spring Playground!";
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Hello, this is Spring Playground!");
     }
 
 }
