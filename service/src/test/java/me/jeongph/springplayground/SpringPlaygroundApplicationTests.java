9699c63a4df03e3f27e3379eb674ab7c09016de2
JeongUk Park <dnr1105@gmail.com>
2024-02-27 22:57:03 +0900

    feat: 궁금증 메모

--- service/src/test/java/me/jeongph/springplayground/SpringPlaygroundApplicationTests.java
+++ service/src/test/java/me/jeongph/springplayground/SpringPlaygroundApplicationTests.java
@@ -1,15 +1,48 @@
 package me.jeongph.springplayground;
 
 import me.jeongph.playground.SpringPlaygroundApplication;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = SpringPlaygroundApplication.class)
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//@SpringBootTest(classes = SpringPlaygroundApplication.class)
 class SpringPlaygroundApplicationTests {
 
     @Test
     void contextLoads() {
 
+    }
+
+    @Test
+    @DisplayName("자바에서 .toList 를 하면 ImmutableCollections 형의 리스트를 반환한다.")
+    void test() {
+        // given
+        var list = List.of("", "");
+
+        // when
+        var actual = list.stream().map(it -> it).toList();
+
+        // then
+        assertEquals(List.of().getClass(), actual.getClass());
+    }
+
+    @Test
+    @DisplayName("자바에서 .collect(Collectors.toList()) 를 하면 ArrayList 형의 리스트를 반환한다.")
+    void test2() {
+        // given
+        var list = List.of("", "");
+
+        // when
+        var actual = list.stream().map(it -> it).collect(Collectors.toList());
+
+        // then
+        assertEquals(ArrayList.class, actual.getClass());
     }
 
 }
